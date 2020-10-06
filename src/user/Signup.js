e67141472ad28c14c9782e9b7f5add8e017baae2
Arman <inzamam.arman@gmail.com>
2020-10-06 21:18:44 +0600

    signup related work error and succes

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,9 +1,10 @@
 import React ,{useState} from 'react';
+import {Link} from 'react-router-dom';
 import Layout from '../core/Layout'
-import {API} from '../config'
+import {API} from '../config';
+import {signup} from '../auth/index'
 
-const Signup=()=>{
-
+const Signup=()=>{  
     const [values, setValues] = useState({
         name:'',
         email: '',
@@ -12,48 +13,79 @@ const Signup=()=>{
         success: false
     });
 
+
     const { name, email, password, success, error } = values;
+
 
     const handleChange = name => event => {
         setValues({ ...values, error: false, [name]: event.target.value });
     };
-    const signup=(user)=>{
-        fetch(`${API}/signup`, {
-            method:"POST",
-            headers:{
-                Accept:'application/json',
-                "Content-Type":"application/json"
-            },
-            body:JSON.stringify(user)
-        })
-        .then((resp)=>{
-            return resp.json()
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
-    }
+    
+    
 
     const clickSubmit = event => {
         event.preventDefault();
+        setValues({...values,error:false})
         signup({ name, email, password })
+        .then((data)=>{
+            if(data.error){
+                setValues({...values,error:data.error,success:false})
+            }
+            else{
+                
+                setValues({...values,     
+                name:'',
+                email: '',
+                password: '',
+                error: '',
+                success: true})
+            }
+
+        })
     };
+
+
+const blurChange=(name)=>(event)=>{
+   // console.log(event.target.id)
+    if(event.target.id ==="name" && event.target.value ===''){
+        //console.log(event.target.id)
+        setValues({...values,error:event.target.id +' required ',success:false})
+        showError();
+    }  
+
+    if( event.target.id ==="email" && event.target.value ===''){
+        //console.log(event.target.id)
+        setValues({...values,error:event.target.id +' required ',success:false})
+        showError();
+    }  
+    if( event.target.id ==="password" && event.target.value ===''){
+        //console.log(event.target.id)
+        setValues({...values,error:event.target.id +' required ',success:false})
+        showError();
+    }  
+
+
+
+}
+
+
+
 
     const signUpForm = () => (
         <form >
             <div className="form-group " >
                 <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control" value={name} />
+                <input id="name" onChange={handleChange('name')} onBlur={blurChange('name')} type="text" className="form-control" value={name} />
             </div>
 
             <div className="form-group">
                 <label className="text-muted">Email</label>
-                <input onChange={handleChange('email')} type="email" className="form-control" value={email} />
+                <input id="email" onChange={handleChange('email')} type="email" onBlur={blurChange('email')} className="form-control" value={email} />
             </div>
 
             <div className="form-group ">
                 <label className="text-muted">Password</label>
-                <input onChange={handleChange('password')} type="password" className="form-control" value={password} />
+                <input id="password" onChange={handleChange('password')} type="password"  onBlur={blurChange('password')} className="form-control" value={password} />
             </div>
             <button onClick={clickSubmit} className="btn btn-primary ">
                 Submit
@@ -61,15 +93,27 @@ const Signup=()=>{
         </form>
     );
 
+    const showError = () => (
+        <div className="alert alert-danger col-md-3" style={{ display: error ? '' : 'none' }}>
+            {error}
+        </div>
+    );
+
+    const showSuccess = () => (
+        <div className="alert alert-info col-md-3" style={{ display: success ? '' : 'none' }}>
+            New account is created. Please <Link to="/signin">Signin</Link>
+        </div>
+    );
+
     return (
         <Layout
             title="Signup"
             description="Signup to Node React E-commerce App"
             className="container col-md-8 offset-md-2"
-        >
-        
-            {signUpForm()}
-            {JSON.stringify(values)}
+        > 
+            {showSuccess()}
+            {showError()}         
+            {signUpForm()}                   
         </Layout>
     );
 
@@ -77,3 +121,4 @@ const Signup=()=>{
 
 
 export default Signup;
+
