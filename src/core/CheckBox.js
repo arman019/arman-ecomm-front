4fe3e74a3e3620cc98f24a9d0a8120c8b73efd67
Arman <inzamam.arman@gmail.com>
2020-10-12 02:03:38 +0600

    checkbox category in shop

--- src/core/CheckBox.js
+++ src/core/CheckBox.js
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 const CheckBox = ({categories}) =>{
+    const [checked, setChecked] = useState([]);
+
+    const handleToggle = c => ()=>{
+        const currentCategoryId = checked.indexOf(c);
+        const newCheckedCategoryId = [...checked];
+
+        if(currentCategoryId === -1){
+            newCheckedCategoryId.push(c);
+        }
+        else{
+            newCheckedCategoryId.splice(currentCategoryId,1);
+        }
+
+        console.log(newCheckedCategoryId)
+        setChecked(newCheckedCategoryId);
+    }
+
     return categories.map((category,i)=>(
         <li key={i} className="list-unstyled">
-            <input type="checkbox" className="form-check-input" />
+            <input onChange={handleToggle(category._id)} type="checkbox" className="form-check-input" value={checked.indexOf(category._id === -1)} />
             <label className="form-check-label">{category.name}  </label>         
         </li>
     ));
